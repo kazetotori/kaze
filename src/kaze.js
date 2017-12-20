f3db880b3cf2e034608280622ed231abbf0659af
kazetotori <kakkouto98045@live.com>
2017-12-20 22:56:18 +0800

    2017-12-20

--- src/kaze.js
+++ src/kaze.js
@@ -1,10 +1,12 @@
  /*
-     Date: 2017-12-20
-     Author: kazetotori/fxqn
-     Description: This file will export Kaze modules
- */
+       Date: 2017-12-20
+       Author: kazetotori/fxqn
+       Description: This file will export Kaze modules
+   */
 
  const Kaze = require('./core/Factory')
+ const YamlConfigLoader = require('./core/YamlConfigLoader')
 
  module.exports = Kaze
  module.exports.Kaze = Kaze
+ module.exports.YamlConfigLoader = YamlConfigLoader
